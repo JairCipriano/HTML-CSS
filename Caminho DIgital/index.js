497ff6dbfcfcdd26a3c419195f991b88d2f178da
JairCipriano <jaircipriano69@gmail.com>
2024-10-24 12:12:10 -0300

    atualizacao

---
+++ Caminho DIgital/index.js
@@ -0,0 +1,4 @@
+// Adiciona um evento de clique ao botão
+document.getElementById('faleConosco').addEventListener('click', function() {
+    alert('Obrigado por entrar em contato!'); // Mensagem ao clicar
+});
